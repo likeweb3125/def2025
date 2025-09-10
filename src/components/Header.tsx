d3c32e3d7859839ce9ceaad450e5adeab43e82a3
heosuyeon <hsy121891@gmail.com>
2025-09-10 20:16:33 +0900

    style: 햄버거 메뉴 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,15 @@ import { usePathname } from "next/navigation";
 import { useLanguage } from "@/contexts/LanguageContext";
 
 import LanguageIcon from "/public/images/icons/language.svg?component";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 export default function Header() {
   const { t, language, setLanguage } = useLanguage();
@@ -26,11 +35,44 @@ export default function Header() {
     setIsLanguageDropdownOpen(!isLanguageDropdownOpen);
   };
 
+  const menuList = [
+    {
+      title: t.nav.eventIntro,
+      href: "/event-overview",
+      subList: [
+        {
+          title: t.nav.eventOverview,
+          href: "/event-overview",
+        },
+        {
+          title: t.nav.pastEvents,
+          href: "/past-events",
+        },
+        {
+          title: t.nav.directions,
+          href: "/location",
+        },
+      ],
+    },
+    {
+      title: t.nav.program,
+      href: "/program",
+    },
+    {
+      title: t.nav.speakers,
+      href: "/speakers",
+    },
+    {
+      title: t.nav.registration,
+      href: "https://naver.com",
+    },
+  ];
+
   return (
     <header className="relative z-50 bg-white sticky top-0">
-      <div className="flex max-w-[1920px] mx-auto px-[100px] max-[1280px]:pl-0 max-[1280px]:pr-10">
+      <div className="flex max-w-[1920px] mx-auto px-[100px] max-[1280px]:pl-0 max-[1280px]:pr-9 max-[428px]:pr-4">
         {/* 왼쪽 로고 블록 */}
-        <div className="relative min-w-[280px] max-[1000px]:min-w-[160px] max-[768px]:translate-x-10">
+        <div className="relative min-w-[280px] max-[1000px]:min-w-[160px] max-[768px]:translate-x-10 max-[428px]:translate-x-5">
           <div className="flex items-center absolute top-0 left-0 inset-0 min-h-[160px] max-[1000px]:min-h-[100px] bg-[linear-gradient(123.75deg,#055DA5_2.12%,#02243F_100%)] overflow-hidden rounded-br-2xl rounded-bl-2xl flex justify-center items-center shadow-[8px_8px_24px_0px_#00000040]">
             <h1>
               <Link
@@ -129,10 +171,10 @@ export default function Header() {
             </Link>
           </nav>
 
-          <div className="max-[768px]:ml-auto flex items-center gap-5">
+          <div className="max-[768px]:ml-auto flex items-center gap-4">
             {/* 언어 선택 */}
             <div
-              className={`relative border rounded-[30px] ${
+              className={`relative border rounded-[30px] max-[428px]:hidden ${
                 isLanguageDropdownOpen
                   ? "border-[#055DA5] "
                   : "border-transparent"
@@ -190,15 +232,115 @@ export default function Header() {
               )}
             </div>
 
-            <button className="max-[768px]:block hidden w-10 h-10 flex items-center justify-center cursor-pointer">
-              <span className="sr-only">메뉴</span>
-              <Image
-                src="/images/icons/menu.png"
-                alt="메뉴"
-                width={40}
-                height={40}
-              />
-            </button>
+            <Sheet>
+              <SheetTrigger asChild className="max-[768px]:block hidden">
+                <button className="w-[46px] h-[46px] flex items-center justify-center cursor-pointer">
+                  <span className="sr-only">메뉴</span>
+                  <Image
+                    src="/images/icons/menu.png"
+                    alt="메뉴"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+              </SheetTrigger>
+              <SheetContent
+                className="gap-0 w-full !max-w-full border-none bg-cover bg-center bg-no-repeat bg-[url('/images/heroes/hero-main-768.png')] max-[460px]:bg-[url('/images/heroes/hero-main-360.png')]"
+                showCloseButton={false}
+              >
+                <SheetHeader className="p-0">
+                  <SheetTitle hidden />
+                  <SheetDescription hidden />
+
+                  <div className="px-10 py-[18px] flex items-center justify-between max-[360px]:px-5 max-[360px]:py-2">
+                    <h2>
+                      <Link
+                        href="/"
+                        className="bg-cover bg-center bg-no-repeat bg-[url('/images/logos/logo_white.png')] w-[74px] h-[36px] block"
+                      >
+                        <span className="sr-only">
+                          DEF - Digital Economy Forum 2025
+                        </span>
+                      </Link>
+                    </h2>
+
+                    <div className="flex items-center gap-3">
+                      <button
+                        onClick={() => setLanguage("ko")}
+                        className={`text-[18px] cursor-pointer ${
+                          language === "ko"
+                            ? "text-white underline"
+                            : "text-white/40"
+                        }`}
+                      >
+                        KOR
+                      </button>
+                      <button
+                        onClick={() => setLanguage("en")}
+                        className={`text-[18px] cursor-pointer ${
+                          language === "en"
+                            ? "text-white underline"
+                            : "text-white/40"
+                        }`}
+                      >
+                        ENG
+                      </button>
+                      <SheetClose asChild>
+                        <button className="ml-2 w-10 h-10 cursor-pointer">
+                          <Image
+                            src="/images/icons/close_white.png"
+                            alt="Close"
+                            width={40}
+                            height={40}
+                          />
+                        </button>
+                      </SheetClose>
+                    </div>
+                  </div>
+                </SheetHeader>
+
+                <ul className="px-5 flex-1 pt-12 flex flex-col items-center gap-[60px] overflow-y-auto max-[360px]:gap-y-12">
+                  {menuList.map((menu) => {
+                    return (
+                      <li key={menu.title}>
+                        <Link
+                          href={menu.href}
+                          className="text-[48px] text-white font-bold max-[360px]:text-[32px] max-[360px]:leading-[37px] text-center block"
+                        >
+                          {menu.title}
+                        </Link>
+                        {menu.subList && (
+                          <ul className="mt-7 flex flex-col items-center justify-center max-[360px]:mt-5">
+                            {menu.subList.map((sub) => {
+                              return (
+                                <li key={sub.title}>
+                                  <Link
+                                    href={sub.href}
+                                    className="text-[28px] text-white font-[500] py-3 block max-[360px]:text-[20px] max-[360px]:leading-[23px] max-[360px]:py-[10px]"
+                                  >
+                                    {sub.title}
+                                  </Link>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+
+                <div className="flex justify-center px-4 py-10">
+                  <Image
+                    src="/images/host_org.png"
+                    alt="Hosted by Ministry of Economy and Finance, Organized by KIET"
+                    width={800}
+                    height={120}
+                    className="w-full max-w-xs md:max-w-md lg:max-w-2xl h-auto object-contain"
+                  />
+                </div>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </div>
